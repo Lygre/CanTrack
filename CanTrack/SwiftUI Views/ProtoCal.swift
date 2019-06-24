30ea14d99a780d4a5add34fec0a06b97443e7cbe
Hugh Broome <hughdylanbroome@gmail.com>
2019-06-23 21:34:56 -0400

    Year Scroll list with range of years now

--- CanTrack/SwiftUI Views/ProtoCal.swift
+++ CanTrack/SwiftUI Views/ProtoCal.swift
@@ -36,15 +36,12 @@ struct ProtoYearView: View {
 	var body: some View {
 		NavigationView {
 			ScrollView(isScrollEnabled: true, showsVerticalIndicator: true) {
-				VStack(alignment: .leading) {
-					Text(CalendarStore.currentDate.year.description)
-						.font(.title)
-						.fontWeight(.heavy)
-						.padding(.leading)
-					Divider()
-					Text("The end is nigh and the dark is full of terrors")
+				VStack {
+					ForEach(2017...2025) { year in
+						YearCellView(year: year)
 					}
-					.scaledToFill()
+
+				}
 
 				}.navigationBarItems(trailing: HStack(spacing: 9) {
 					Image(systemName: "magnifyingglass")
@@ -59,16 +56,42 @@ struct ProtoYearView: View {
 	}
 }
 
-#if DEBUG
-struct ProtoCal_Previews : PreviewProvider {
-    static var previews: some View {
-        ProtoCal().environmentObject(calendarStore)
-    }
+
+struct YearCellView : View {
+
+	var year: Int
+
+	var body: some View {
+		VStack(alignment: .leading) {
+			Text(year.description)
+				.font(.title)
+				.fontWeight(.heavy)
+
+			Divider()
+			.padding(.leading)
+			Text("The end is nigh and the dark is full of terrors")
+			}
+			.scaledToFill()
+	}
 }
+
+#if DEBUG
+//struct ProtoCal_Previews : PreviewProvider {
+//    static var previews: some View {
+//        ProtoCal().environmentObject(calendarStore)
+//    }
+//}
 
 struct ProtoYearView_Previews : PreviewProvider {
 	static var previews: some View {
 		ProtoYearView().environmentObject(calendarStore)
 	}
 }
+
+struct YearCellView_Previews : PreviewProvider {
+	static var previews: some View {
+		YearCellView(year: CalendarStore.currentDate.year).environmentObject(calendarStore)
+	}
+}
 #endif
+

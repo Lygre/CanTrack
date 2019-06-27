176e54201db259a4d6d7dba41bf63bdf3fbe7a55
Hugh Broome <hughdylanbroome@gmail.com>
2019-06-27 14:43:58 -0400

    Init commit for watchOS Stadalone app version

---
+++ CanTrackWatch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  CanTrackWatch WatchKit Extension
+//
+//  Created by Hugh Broome on 6/27/19.
+//  Copyright © 2019 Lygre. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ CanTrackWatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  CanTrackWatch WatchKit Extension
+//
+//  Created by Hugh Broome on 6/27/19.
+//  Copyright © 2019 Lygre. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews : PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif

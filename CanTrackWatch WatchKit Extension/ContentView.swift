20e166259d5c944be9894b42c1d419df6431b908
Hugh Broome <hughdylanbroome@gmail.com>
2019-06-27 22:58:37 -0400

    Successful build with standalone watch, does display content. Preparing view hierarchy for environment object injection

--- CanTrackWatch WatchKit Extension/ContentView.swift
+++ CanTrackWatch WatchKit Extension/ContentView.swift
@@ -8,16 +8,31 @@
 
 import SwiftUI
 
+let testData2: [String: AnyObject] =
+	["strains": StrainStore(strains: [StrainStore.defaultStrain]),
+	 "products": defaultProducts
+]
+
 struct ContentView : View {
+
+	var productStore: ProductStore
+	var strainStore: StrainStore
+
     var body: some View {
-        Text("Hello World")
+        HStack {
+            Text("Hello World")
+			ProductImageViewCircular(product: productStore.products[0])
+        }
     }
 }
 
 #if DEBUG
+
+
+
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+		ContentView(productStore: testData2["products"] as! ProductStore, strainStore: testData2["strains"] as! StrainStore)
     }
 }
 #endif
--- CanTrackWatch WatchKit Extension/HostingController.swift
+++ CanTrackWatch WatchKit Extension/HostingController.swift
@@ -10,8 +10,17 @@ import WatchKit
 import Foundation
 import SwiftUI
 
+let testData: [String: AnyObject] =
+	["strains": StrainStore(strains: [StrainStore.defaultStrain]),
+	 "products": defaultProducts
+]
+
 class HostingController : WKHostingController<ContentView> {
+
+	var strainStore: StrainStore = testData["strains"] as! StrainStore
+	var productStore: ProductStore = testData["products"] as! ProductStore
+	
     override var body: ContentView {
-        return ContentView()
+		return ContentView(productStore: productStore, strainStore: strainStore)
     }
 }

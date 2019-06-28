e7851cd87bfa83dac0db7d09e712aa26d1123853
Hugh Broome <hughdylanbroome@gmail.com>
2019-06-28 13:41:57 -0400

    --WatchProductView.swift: • added isOpened state variable for an optional view to be presented based on whether the product is opened or not • added dateFormatter constant • added some additional view modifiers to clean the layout up a bit.

--- CanTrackWatch WatchKit Extension/Watch Views/WatchProductView.swift
+++ CanTrackWatch WatchKit Extension/Watch Views/WatchProductView.swift
@@ -7,24 +7,45 @@
 //
 
 import SwiftUI
+import Foundation
+
 
 struct WatchProductView : View {
 
 	var product: Product
 
+	@State private var isOpened: Bool = false
+
+	let dateFormatter: DateFormatter = {
+		let dateFormatter = DateFormatter()
+		dateFormatter.timeZone = .current
+		dateFormatter.locale = .current
+		dateFormatter.calendar = .current
+		dateFormatter.dateFormat = "d"
+		return dateFormatter
+	}()
+
     var body: some View {
-        VStack {
+		VStack(alignment: .leading) {
             Text(product.productType.rawValue)
-			Divider()
+				.lineLimit(nil)
 			Text(product.strain.name)
-			.lineLimit(nil)
+				.lineLimit(nil)
+			Text(product.mass+" g")
+			isOpened ? Text(dateFormatter.string(from: product.dateOpened!)) : Text("Unopened")
         }
+		.background(Color.green)
     }
 }
 
 #if DEBUG
 
-let testProd = defaultProducts.products[0]
+var testProd: Product = {
+	var prod = Product(strain: StrainStore.defaultStrain, productType: .truFlower)
+	prod.dateOpened = Date()
+	return prod
+}()
+
 
 struct WatchProductView_Previews : PreviewProvider {
     static var previews: some View {

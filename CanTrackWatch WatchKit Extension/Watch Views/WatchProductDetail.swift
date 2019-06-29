800cfe9432d8c39d01fed80fece2905dd8aec37f
Hugh Broome <hughdylanbroome@gmail.com>
2019-06-29 04:00:09 -0400

    --WatchProductDetial.swift: • Added @EnvironmentObject variable productStore • added productIndex property, for manipulating to Model data • Lotsa layout changes, but now we have a functional favorite toggle button

--- CanTrackWatch WatchKit Extension/Watch Views/WatchProductDetail.swift
+++ CanTrackWatch WatchKit Extension/Watch Views/WatchProductDetail.swift
@@ -9,10 +9,15 @@
 import SwiftUI
 
 struct WatchProductDetail : View {
+	@EnvironmentObject var productStore: ProductStore
 
 	@State private var isOpened: Bool = true
 
 	var product: Product
+
+	var productIndex: Int {
+		productStore.products.firstIndex(where: { $0.id == product.id })!
+	}
 
 	private let dateFormatter: DateFormatter = {
 		let dateFormatter2 = DateFormatter()
@@ -26,19 +31,29 @@ struct WatchProductDetail : View {
 
 
 	var body: some View {
-		HStack(alignment: .bottom) {
+		HStack {
 			VStack(alignment: .leading, spacing: 5) {
 				HStack {
 					Text(product.productType.rawValue)
-					.lineLimit(nil)
-					
-					Image(systemName: "star.fill")
+						.lineLimit(nil)
+					Button(action: {
+						self.productStore.products[self.productIndex].isFavorite.toggle()
+					}) {
+						if self.productStore.products[self.productIndex].isFavorite {
+							Image(systemName: "star.fill")
+								.foregroundColor(Color.yellow)
+						} else {
+							Image(systemName: "star")
+								.foregroundColor(Color.gray)
+						}
+					}
 				}
 				Text("Mass:"+" \(product.mass)g")
-				isOpened ? Text("Opened: "+dateFormatter.string(from: product.dateOpened ?? Date())) : Text("Unopened")
+				isOpened ? Text("Opened: "+dateFormatter.string(from: product.dateOpened ?? Date()))
+					.lineLimit(nil): Text("Unopened")
+						.lineLimit(nil)
 				Spacer()
-			}
-			Spacer()
+				}
 				.navigationBarTitle(Text(product.strain.name))
 		}
 	}
@@ -47,7 +62,7 @@ struct WatchProductDetail : View {
 #if DEBUG
 struct WatchProductDetail_Previews : PreviewProvider {
     static var previews: some View {
-		WatchProductDetail(product: defaultProducts.products[0])
+		WatchProductDetail(product: defaultProducts.products[0]).environmentObject(productStore)
     }
 }
 #endif

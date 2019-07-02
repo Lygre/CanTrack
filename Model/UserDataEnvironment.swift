27af38af9e8750e052f7b897d513fb3336e1a330
Hugh Broome <hughdylanbroome@gmail.com>
2019-07-02 07:51:02 -0400

    --Got UserData class added back with watch still building

---
+++ Model/UserDataEnvironment.swift
@@ -0,0 +1,17 @@
+//
+//  UserDataEnvironment.swift
+//  CanTrack
+//
+//  Created by Hugh Broome on 7/2/19.
+//  Copyright © 2019 Lygre. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+	let didChange = PassthroughSubject<UserData, Never>()
+
+	var doses: [Dose] = []
+
+}

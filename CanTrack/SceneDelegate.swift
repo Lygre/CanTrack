72cd988d4370f5488ebe9a9bb84aa2e95595c742
Hugh Broome <hughdylanbroome@gmail.com>
2019-07-02 09:49:53 -0400

    Converted over to userData from ProductStore in iOS/macOS/iPadOS SceneDelegate

--- CanTrack/SceneDelegate.swift
+++ CanTrack/SceneDelegate.swift
@@ -12,8 +12,7 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	var window: UIWindow?
-//	var userData: UserData = UserData()
-	var productStore: ProductStore = ProductStore(products: testData)
+	var userData: UserData = UserData()
 	var strainStore: StrainStore = StrainStore(strains: testData.compactMap({ return $0.strain }))
 	var calendarStore: CalendarStore = CalendarStore()
 
@@ -30,7 +29,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		let window = UIWindow(frame: UIScreen.main.bounds)
 		#endif
 
-		window.rootViewController = UIHostingController(rootView: HomeView().environmentObject(productStore).environmentObject(strainStore).environmentObject(calendarStore))
+		window.rootViewController = UIHostingController(rootView: HomeView().environmentObject(userData).environmentObject(strainStore).environmentObject(calendarStore))
 		self.window = window
 		window.makeKeyAndVisible()
 	}
